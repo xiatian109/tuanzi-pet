3f93c90df2abf131445cbd7c15ef303750af86f8
xiatian109 <13437034991@163.com>
2026-08-07 13:29:23 +0800

    fix: MainActivity 引导开启文件访问权限

--- app/src/main/java/com/tuanzi/pet/MainActivity.kt
+++ app/src/main/java/com/tuanzi/pet/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.Settings
 import android.widget.Button
 import android.widget.Toast
@@ -18,6 +19,20 @@ class MainActivity : Activity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.btn_start).setOnClickListener {
+            // 先确保有"所有文件访问"权限，否则无论如何都读不到 /sdcard/Download/mood_state.json
+            if (!Environment.isExternalStorageManager()) {
+                try {
+                    startActivity(
+                        Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+                            .setData(Uri.parse("package:$packageName"))
+                    )
+                } catch (_: Exception) {
+                    startActivity(Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION))
+                }
+                Toast.makeText(this, "请先授予「所有文件访问」权限，否则团子读不到你的心情", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
             if (Settings.canDrawOverlays(this)) {
                 startForegroundService(Intent(this, OverlayService::class.java))
             } else {
